5fde20792f487222e7f44931260632cbaf4f169e
sophiajw <sophia.wagner@t-online.de>
2024-04-08 04:40:54 +0200

    add tinting as augmentation

--- dinov2/data/augmentations.py
+++ dinov2/data/augmentations.py
@@ -9,55 +9,21 @@ from typing import Callable
 # from skimage.exposure import rescale_intensity
 import numpy as np
 # from skimage.util import dtype, dtype_limits
-from PIL import Image
+from PIL import Image, ImageChops
 from torchvision import transforms
 import random
 from .transforms import GaussianBlur, make_normalize_transform
 
 logger = logging.getLogger("dinov2")
 
-# class HEDJitter(Callable):
-#     # HED color augmentations
-#     # adapted from  https://github.com/DIAGNijmegen/pathology-he-auto-augment/blob/main/he-randaugment/custom_hed_transform.py
-#     def __init__(self, factor=0.07):
-#         self.factor = factor
 
-#         self.rgb_from_hed = np.array([[0.65, 0.70, 0.29],
-#                             [0.07, 0.99, 0.11],
-#                             [0.27, 0.57, 0.78]]).astype('float32')
-
-#         self.hed_from_rgb = np.linalg.inv(self.rgb_from_hed).astype('float32')
-
-#     def rgb2hed(self, rgb):
-#         rgb = dtype.img_as_float(rgb, force_copy=True).astype('float32')
-#         rgb += 2
-#         stains = np.dot(np.reshape(-np.log(rgb), (-1, 3)), self.hed_from_rgb)
-#         return np.reshape(stains, rgb.shape)
-
-#     def hed2rgb(self, hed):
-#         stains = dtype.img_as_float(hed.astype('float64')).astype('float32')  # stains are out of range [-1, 1] so dtype.img_as_float complains if not float64
-#         logrgb2 = np.dot(-np.reshape(stains, (-1, 3)), self.rgb_from_hed)
-#         rgb2 = np.exp(logrgb2)
-#         return rescale_intensity(np.reshape(rgb2 - 2, stains.shape),
-#                                 in_range=(-1, 1))
-
-#     def __call__(self, patch):
-#         __cutoff_range = (0.15, 0.85)
-#         __biases = [np.random.uniform(-self.factor, self.factor) for _ in range(3)]
-#         __sigmas = [np.random.uniform(-self.factor, self.factor) for _ in range(3)]
-
-#         patch_hed = self.rgb2hed(np.array(patch))
-
-#         patch_hed *= (1.0 + np.array(__sigmas))
-#         patch_hed += np.array(__biases)
-
-#         patch_rgb = self.hed2rgb(hed=patch_hed)
-#         patch_rgb = np.clip(a=patch_rgb, a_min=0.0, a_max=1.0)
-#         patch_rgb *= 255.0
-#         patch_rgb = patch_rgb.astype(dtype=np.uint8)
-
-#         return Image.fromarray(patch_rgb)
-
+class TintImage(object):
+    def __init__(self, tint_color_range=((0, 255), (0, 255), (0, 255))):
+        self.tint_color_range = tint_color_range
+    
+    def __call__(self, image):
+        tint_color = tuple(np.random.randint(low, high) for low, high in self.tint_color_range)
+        return ImageChops.multiply(image, Image.new('RGB', image.size, tint_color))
 
 class DataAugmentationDINO(object):
     def __init__(
@@ -113,15 +79,12 @@ class DataAugmentationDINO(object):
         # color distorsions / blurring
         color_jittering = transforms.Compose(
             [
-                #        transforms.RandomApply(
-                #            [HEDJitter(factor=0.07)],
-                #            p=0.5,
-                #        ),
+                transforms.RandomApply([TintImage(((145, 155), (175, 185), (150, 180)))], p=0.05),  # blue tint
+                transforms.RandomApply([TintImage(((225, 255), (200, 230), (180, 210)))], p=0.5),  # yellow/red tint
                 transforms.RandomApply(
                     [transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1)],
                     p=0.8,  # original p=0.8
                 ),
-                # additional histopathology-specific augmentations (don't use grayscale)
                 transforms.RandomGrayscale(p=0.2),  # original p=0.2, changed
             ]
         )
