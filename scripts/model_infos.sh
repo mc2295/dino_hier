fd24b15d30cc2abeb92741b282baec8f7c7caa6e
sophiajw <sophia.wagner@t-online.de>
2025-02-10 16:22:40 +0100

    updated eval scripts

--- scripts/model_infos.sh
+++ scripts/model_infos.sh
@@ -1,77 +1,31 @@
-# superbloom L
-MODEL_PATH=/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vitl_8_new_supcon/eval/training_4999
-CKPT_PATH=/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vitl_8_new_supcon/eval/training_4999/teacher_checkpoint.pth
-MODEL_ROOT=/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vitl_8_new_supcon
-MODEL_NAME=dinov2_vitl14
-EXP_NAME=superbloom_vitl_8_new_supcon_4999
+#!/bin/bash
+# Define models in an array.
+# Format for each element: 
+# "description|MODEL_PATH|MODEL_ROOT|MODEL_NAME|EXP_NAME|CKPT_PATH"
+MODELS=(
+  # "superbloom L|/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vitl_8_new_supcon/eval/training_4999|/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vitl_8_new_supcon|dinov2_vitl14|superbloom_vitl_8_new_supcon_4999|/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vitl_8_new_supcon/eval/training_4999/teacher_checkpoint.pth"
+  
+  # "superbloom S|/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vits_8/eval/training_5999|/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vits_8|dinov2_vits14|superbloom_s_5999|/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vits_8/eval/training_5999/teacher_checkpoint.pth"
+  
+  # "superbloom S (new supcon) 5999|/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vits_8_new_supcon/eval/training_5999|/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vits_8_new_supcon|dinov2_vits14|superbloom_s_new_supcon_5999|/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vits_8_new_supcon/eval/training_5999/teacher_checkpoint.pth"
+  
+  # "superbloom S (new supcon) 7999|/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vits_8_new_supcon/eval/training_7999|/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vits_8_new_supcon|dinov2_vits14|superbloom_s_new_supcon_7999|/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vits_8_new_supcon/eval/training_7999/teacher_checkpoint.pth"
+  
+  # "superbloom B|/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vitb_8/eval/training_6999|/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vitb_8|dinov2_vitb14|superbloom_vitb_8_6999|/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vitb_8/eval/training_6999/teacher_checkpoint.pth"
+  
+  # "superbloom B (new supcon)|/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vitb_8_new_supcon/eval/training_7999|/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vitb_8_new_supcon|dinov2_vitb14|superbloom_b_new_supcon_7999|/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vitb_8_new_supcon/eval/training_7999/teacher_checkpoint.pth"
+  
+  # "vits_3M_350k_bs416_0.1ce+supcon_mlp_rbc 4999|/lustre/groups/shared/users/peng_marr/DinoBloomv2/vits_3M_350k_bs416_0.1ce+supcon_mlp_rbc/eval/training_4999|/lustre/groups/shared/users/peng_marr/DinoBloomv2/vits_3M_350k_bs416_0.1ce+supcon_mlp_rbc|dinov2_vits14|vits_3M_350k_bs416_0.1ce+supcon_mlp_rbc_4999|/lustre/groups/shared/users/peng_marr/DinoBloomv2/vits_3M_350k_bs416_0.1ce+supcon_mlp_rbc/eval/training_4999/teacher_checkpoint.pth"
+  
+  # "vits_3M_350k_bs416_0.1ce+supcon_mlp_rbc 7999|/lustre/groups/shared/users/peng_marr/DinoBloomv2/vits_3M_350k_bs416_0.1ce+supcon_mlp_rbc/eval/training_7999|/lustre/groups/shared/users/peng_marr/DinoBloomv2/vits_3M_350k_bs416_0.1ce+supcon_mlp_rbc|dinov2_vits14|vits_3M_350k_bs416_0.1ce+supcon_mlp_rbc_7999|/lustre/groups/shared/users/peng_marr/DinoBloomv2/vits_3M_350k_bs416_0.1ce+supcon_mlp_rbc/eval/training_7999/teacher_checkpoint.pth"
+  
+  # "vits_3M_350k_bs416_0.1ce+new_supcon_mlp_rbc 7999|/lustre/groups/shared/users/peng_marr/DinoBloomv2/vits_3M_350k_bs416_0.1ce+new_supcon_mlp_rbc/eval/training_7999|/lustre/groups/shared/users/peng_marr/DinoBloomv2/vits_3M_350k_bs416_0.1ce+new_supcon_mlp_rbc|dinov2_vits14|vits_3M_350k_bs416_0.1ce+new_supcon_mlp_rbc_7999|/lustre/groups/shared/users/peng_marr/DinoBloomv2/vits_3M_350k_bs416_0.1ce+new_supcon_mlp_rbc/eval/training_7999/teacher_checkpoint.pth"
+  
+  # "conch|/lustre/groups/shared/users/peng_marr/DinoBloomv2/conch/pytorch_model.bin|/lustre/groups/shared/users/peng_marr/DinoBloomv2/conch|conch|conch|/lustre/groups/shared/users/peng_marr/DinoBloomv2/conch/pytorch_model.bin"
 
-# superbloom S
-MODEL_PATH=/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vits_8/eval/training_5999
-CKPT_PATH=/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vits_8/eval/training_5999/teacher_checkpoint.pth
-MODEL_ROOT=/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vits_8
-MODEL_NAME=dinov2_vits14
-EXP_NAME=superbloom_s_5999
+  "conchv1.5|/lustre/groups/shared/users/peng_marr/DinoBloomv2/conchv1.5/conch_v1_5_pytorch_model.bin|/lustre/groups/shared/users/peng_marr/DinoBloomv2/conchv1.5|conchv1.5|conchv1.5|/lustre/groups/shared/users/peng_marr/DinoBloomv2/conchv1.5/conch_v1_5_pytorch_model.bin"
 
-# superbloom S (new supcon) 5999
-MODEL_PATH=/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vits_8_new_supcon/eval/training_5999
-CKPT_PATH=/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vits_8_new_supcon/eval/training_5999/teacher_checkpoint.pth
-MODEL_ROOT=/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vits_8_new_supcon
-MODEL_NAME=dinov2_vits14
-EXP_NAME=superbloom_s_new_supcon_5999
+  # "uni|/lustre/groups/shared/users/peng_marr/DinoBloomv2/uni/pytorch_model.bin|/lustre/groups/shared/users/peng_marr/DinoBloomv2/uni|uni|uni|/lustre/groups/shared/users/peng_marr/DinoBloomv2/uni/pytorch_model.bin"
 
-# superbloom S (new supcon) 7999
-MODEL_PATH=/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vits_8_new_supcon/eval/training_7999
-CKPT_PATH=/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vits_8_new_supcon/eval/training_7999/teacher_checkpoint.pth
-MODEL_ROOT=/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vits_8_new_supcon
-MODEL_NAME=dinov2_vits14
-EXP_NAME=superbloom_s_new_supcon_7999
-
-# superbloom B
-MODEL_PATH=/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vitb_8/eval/training_6999
-CKPT_PATH=/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vitb_8/eval/training_6999/teacher_checkpoint.pth
-MODEL_ROOT=/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vitb_8
-MODEL_NAME=dinov2_vitb14
-EXP_NAME=superbloom_vitb_8_6999
-
-# superbloom B (new supcon)
-MODEL_PATH=/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vitb_8_new_supcon/eval/training_7999
-CKPT_PATH=/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vitb_8_new_supcon/eval/training_7999/teacher_checkpoint.pth
-MODEL_ROOT=/lustre/groups/shared/users/peng_marr/DinoBloomv2/superbloom/vitb_8_new_supcon
-MODEL_NAME=dinov2_vitb14
-EXP_NAME=superbloom_b_new_supcon_7999
-
-# vits_3M_350k_bs416_0.1ce+supcon_mlp_rbc 4999
-MODEL_PATH=/lustre/groups/shared/users/peng_marr/DinoBloomv2/vits_3M_350k_bs416_0.1ce+supcon_mlp_rbc/eval/training_4999
-CKPT_PATH=/lustre/groups/shared/users/peng_marr/DinoBloomv2/vits_3M_350k_bs416_0.1ce+supcon_mlp_rbc/eval/training_4999/teacher_checkpoint.pth
-MODEL_ROOT=/lustre/groups/shared/users/peng_marr/DinoBloomv2/vits_3M_350k_bs416_0.1ce+supcon_mlp_rbc
-MODEL_NAME=dinov2_vits14
-EXP_NAME=vits_3M_350k_bs416_0.1ce+supcon_mlp_rbc_4999
-
-# vits_3M_350k_bs416_0.1ce+supcon_mlp_rbc 7999
-MODEL_PATH=/lustre/groups/shared/users/peng_marr/DinoBloomv2/vits_3M_350k_bs416_0.1ce+supcon_mlp_rbc/eval/training_7999
-CKPT_PATH=/lustre/groups/shared/users/peng_marr/DinoBloomv2/vits_3M_350k_bs416_0.1ce+supcon_mlp_rbc/eval/training_7999/teacher_checkpoint.pth
-MODEL_ROOT=/lustre/groups/shared/users/peng_marr/DinoBloomv2/vits_3M_350k_bs416_0.1ce+supcon_mlp_rbc
-MODEL_NAME=dinov2_vits14
-EXP_NAME=vits_3M_350k_bs416_0.1ce+supcon_mlp_rbc_7999
-
-# vits_3M_350k_bs416_0.1ce+new_supcon_mlp_rbc 7999
-MODEL_PATH=/lustre/groups/shared/users/peng_marr/DinoBloomv2/vits_3M_350k_bs416_0.1ce+new_supcon_mlp_rbc/eval/training_7999
-CKPT_PATH=/lustre/groups/shared/users/peng_marr/DinoBloomv2/vits_3M_350k_bs416_0.1ce+new_supcon_mlp_rbc/eval/training_7999/teacher_checkpoint.pth
-MODEL_ROOT=/lustre/groups/shared/users/peng_marr/DinoBloomv2/vits_3M_350k_bs416_0.1ce+new_supcon_mlp_rbc
-MODEL_NAME=dinov2_vits14
-EXP_NAME=vits_3M_350k_bs416_0.1ce+new_supcon_mlp_rbc_7999
-
-# --- baselines
-# conch
-MODEL_PATH=/lustre/groups/shared/users/peng_marr/DinoBloomv2/conch/pytorch_model.bin
-CKPT_PATH=/lustre/groups/shared/users/peng_marr/DinoBloomv2/conch/pytorch_model.bin
-MODEL_ROOT=/lustre/groups/shared/users/peng_marr/DinoBloomv2/conch
-MODEL_NAME=conch
-EXP_NAME=conch
-
-# uni
-MODEL_PATH=/lustre/groups/shared/users/peng_marr/DinoBloomv2/uni/pytorch_model.bin
-CKPT_PATH=/lustre/groups/shared/users/peng_marr/DinoBloomv2/uni/pytorch_model.bin
-MODEL_ROOT=/lustre/groups/shared/users/peng_marr/DinoBloomv2/uni
-MODEL_NAME=uni
-EXP_NAME=uni
+  "tong hempath|/lustre/groups/shared/users/peng_marr/DinoBloomv2/tong_hempath/eval/training_124999|/lustre/groups/shared/users/peng_marr/DinoBloomv2/tong_hempath|dinov2_vitl16|tong_hempath_124999|/lustre/groups/shared/users/peng_marr/DinoBloomv2/tong_hempath/eval/training_124999/teacher_checkpoint.pth"
+)
--- scripts/run_all_evals.sh
+++ scripts/run_all_evals.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
+set -e
+
+# Source the model definitions
+source scripts/model_infos.sh
 
 # Define common paths and variables
-MODEL_PATH=/lustre/groups/shared/users/peng_marr/DinoBloomv2/vits_3M_350k_bs416_0.1ce+new_supcon_mlp_rbc/eval/training_7999
-CKPT_PATH=/lustre/groups/shared/users/peng_marr/DinoBloomv2/vits_3M_350k_bs416_0.1ce+new_supcon_mlp_rbc/eval/training_7999/teacher_checkpoint.pth
-MODEL_ROOT=/lustre/groups/shared/users/peng_marr/DinoBloomv2/vits_3M_350k_bs416_0.1ce+new_supcon_mlp_rbc
-MODEL_NAME=dinov2_vits14
-EXP_NAME=vits_3M_350k_bs416_0.1ce+new_supcon_mlp_rbc_7999
-
 LOG_DIR=/lustre/groups/shared/users/peng_marr/DinoBloomv2/job_scripts
 PROJECT_PATH=/home/haicu/sophia.wagner/projects/dinov2
 ENV_NAME=superbloom
 
 # Function to submit jobs with common SLURM parameters
-submit_job() {    
+submit_job() {
     sbatch \
-        --output=$LOG_DIR/${EXP_NAME}_log_%j.txt \
-        --error=$LOG_DIR/${EXP_NAME}_error_%j.txt \
-        --job-name=$EXP_NAME \
+        --output="$LOG_DIR/${EXP_NAME}_log_%j.txt" \
+        --error="$LOG_DIR/${EXP_NAME}_error_%j.txt" \
+        --job-name="$EXP_NAME" \
         --partition=gpu_p \
         --qos=gpu_normal \
         --time=8:00:00 \
@@ -25,38 +23,53 @@ submit_job() {
         "$@"
 }
 
-# Run patch eval with fixed splits
-submit_job scripts/run_patch_eval_fixed.sh \
-    "$MODEL_PATH" "$MODEL_ROOT" "$MODEL_NAME" "$EXP_NAME" "bonemarrow" \
-    "dinov2/eval/patch_level/splits/bm_train.csv" \
-    "dinov2/eval/patch_level/splits/bm_test.csv" \
-    "$PROJECT_PATH" "$ENV_NAME"
-
-submit_job scripts/run_patch_eval_fixed.sh \
-    "$MODEL_PATH" "$MODEL_ROOT" "$MODEL_NAME" "$EXP_NAME" "rbc_eval" \
-    "dinov2/eval/patch_level/splits/rbc_train.csv" \
-    "dinov2/eval/patch_level/splits/rbc_test.csv" \
-    "$PROJECT_PATH" "$ENV_NAME"
-
-# Run patch eval with cross-validation
-submit_job scripts/run_patch_eval.sh \
-    "$MODEL_PATH" "$MODEL_ROOT" "$MODEL_NAME" "acevedo_eval" \
-    "/lustre/groups/labs/marr/qscd01/datasets/armingruber/_Domains/Acevedo_cropped" \
-    "dino_eval" "$PROJECT_PATH" "$ENV_NAME"
-
-submit_job scripts/run_patch_eval.sh \
-    "$MODEL_PATH" "$MODEL_ROOT" "$MODEL_NAME" "apl_cell_eval" \
-    "/lustre/groups/shared/histology_data/hematology_data/APL_AML" \
-    "apl_cell_eval" "$PROJECT_PATH" "$ENV_NAME"
-
-submit_job scripts/run_patch_eval.sh \
-    "$MODEL_PATH" "$MODEL_ROOT" "$MODEL_NAME" "raabin_eval" \
-    "/lustre/groups/shared/histology_data/hematology_data/raabin_wbc" \
-    "raabin_eval" "$PROJECT_PATH" "$ENV_NAME"
-
-# Run slide eval
-submit_job scripts/run_mil_eval.sh "APL_AML_all" "WBCMIL" "$CKPT_PATH" "$MODEL_NAME" "$MODEL_ROOT"
-
-submit_job scripts/run_mil_eval.sh "AML_Hehr" "WBCMIL" "$CKPT_PATH" "$MODEL_NAME" "$MODEL_ROOT"
-
-submit_job scripts/run_mil_eval.sh "Beluga" "WBCMIL" "$CKPT_PATH" "$MODEL_NAME" "$MODEL_ROOT"
+# Loop over each model and run all evaluations
+for model_info in "${MODELS[@]}"; do
+    # Split the model_info string using the '|' delimiter
+    IFS="|" read -r model_desc MODEL_PATH MODEL_ROOT MODEL_NAME EXP_NAME CKPT_PATH <<< "$model_info"
+    
+    echo "Submitting evaluations for model: $model_desc ($EXP_NAME)"
+    
+    # === Patch Evaluation: Fixed splits ===
+    
+    # (a) bonemarrow split
+    submit_job scripts/run_patch_eval_fixed.sh \
+        "$MODEL_PATH" "$MODEL_ROOT" "$MODEL_NAME" "$EXP_NAME" "bonemarrow" \
+        "dinov2/eval/patch_level/splits/bm_train.csv" \
+        "dinov2/eval/patch_level/splits/bm_test.csv" \
+        "$PROJECT_PATH" "$ENV_NAME"
+    
+    # (b) rbc_eval split
+    submit_job scripts/run_patch_eval_fixed.sh \
+        "$MODEL_PATH" "$MODEL_ROOT" "$MODEL_NAME" "$EXP_NAME" "rbc_eval" \
+        "dinov2/eval/patch_level/splits/rbc_train.csv" \
+        "dinov2/eval/patch_level/splits/rbc_test.csv" \
+        "$PROJECT_PATH" "$ENV_NAME"
+    
+    # === Patch Evaluation: Cross-Validation ===
+    
+    # (a) Acevedo
+    submit_job scripts/run_patch_eval.sh \
+        "$MODEL_PATH" "$MODEL_ROOT" "$MODEL_NAME" "dino_eval" \
+        "/lustre/groups/labs/marr/qscd01/datasets/armingruber/_Domains/Acevedo_cropped" \
+        "dino_eval" "$PROJECT_PATH" "$ENV_NAME"
+    
+    # (b) APL cell
+    submit_job scripts/run_patch_eval.sh \
+        "$MODEL_PATH" "$MODEL_ROOT" "$MODEL_NAME" "apl_cell_eval" \
+        "/lustre/groups/shared/histology_data/hematology_data/APL_AML" \
+        "apl_cell_eval" "$PROJECT_PATH" "$ENV_NAME"
+    
+    # (c) Raabin
+    submit_job scripts/run_patch_eval.sh \
+        "$MODEL_PATH" "$MODEL_ROOT" "$MODEL_NAME" "raabin_eval" \
+        "/lustre/groups/shared/histology_data/hematology_data/raabin_wbc" \
+        "raabin_eval" "$PROJECT_PATH" "$ENV_NAME"
+    
+    # === Slide Evaluation (MIL) ===
+    
+    submit_job scripts/run_mil_eval.sh "APL_AML_all" "WBCMIL" "$CKPT_PATH" "$MODEL_NAME" "$MODEL_ROOT"
+    submit_job scripts/run_mil_eval.sh "AML_Hehr" "WBCMIL" "$CKPT_PATH" "$MODEL_NAME" "$MODEL_ROOT"
+    submit_job scripts/run_mil_eval.sh "Beluga" "WBCMIL" "$CKPT_PATH" "$MODEL_NAME" "$MODEL_ROOT"
+    
+done
