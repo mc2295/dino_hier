8bb5f915ac58e0b28aa03dfbf82744349f022b72
mc2295 <manon.chossegros@sorbonne-universite.fr>
2025-02-18 16:51:14 +0100

    addition of hierarchical loss function in loss folder

--- dinov2/loss/__init__.py
+++ dinov2/loss/__init__.py
@@ -6,3 +6,4 @@
 from .dino_clstoken_loss import DINOLoss
 from .ibot_patch_loss import iBOTPatchLoss
 from .koleo_loss import KoLeoLoss
+from .hierarchical_ce import HierarchicalCrossEntropyLoss, get_weighting, load_hierarchy
