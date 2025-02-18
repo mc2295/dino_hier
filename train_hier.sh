246adfd31f37d0e07148c7f7d47805c1dba64a0b
mc2295 <manon.chossegros@sorbonne-universite.fr>
2025-02-18 17:20:07 +0100

    create bash file for hierarchical training

---
+++ train_hier.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH -o log/out_hier.txt
+#SBATCH -e log/err_hier.txt
+#SBATCH -J dino_hier
+#SBATCH -p gpu_p
+#SBATCH --mem=300G
+#SBATCH -c 20
+#SBATCH --ntasks=1
+#SBATCH -q gpu_normal
+#SBATCH --time=1:00:00
+#SBATCH --nice=1000
+#SBATCH --gres=gpu:1
+
+
+
+# Environment setup
+source $HOME/.bashrc
+conda activate dinobloom2cond #dinobloom2cond #dinobloom
+export PATH=$CONDA_PREFIX/bin:$PATH
+cd /home/aih/manon.chossegros/from_sophia/dinov2/
+#cd /home/haicu/sophia.wagner/projects/dinov2
+#export CUDA_LAUNCH_BLOCKING=1
+torchrun --nproc_per_node=1 dinov2/train/train.py --no-resume --config-file "dinov2/configs/train/custom_hier.yaml" --output_dir "models/dino_hier"
